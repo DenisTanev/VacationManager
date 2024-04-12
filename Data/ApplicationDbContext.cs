17aad92d4566a7cd7b40caad63c9c3137736cb42
DenisTanev <denistanev@icloud.com>
2024-04-12 17:50:56 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using VacationManager.Models;
+
+namespace VacationManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VacationManager.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+
+
+        /*[DisplayName("Team")]
+        public int? TeamId { get; set; }
+        public Team Team { get; set; }
+
+        [DisplayName("Team led")]
+        public int? TeamLedId { get; set; }
+        public Team TeamLed { get; set; }
+
+
+
+        public IEnumerable<Holiday> RequestedHolidays { get; set; }*/
+
+        [DisplayName("Main role")]
+        [NotMapped]
+        public string MainRole { get; set; }
+    }
+}
